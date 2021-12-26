c060829b10fc80e4a878b40fbc3e3459171b4d30
Batuhan <batuhanmunger@hotmail.com>
2021-12-26 20:14:19 +0300

    Add CRUD functions and Username input

---
+++ Todo.js
@@ -0,0 +1,54 @@
+const nameInput = document.querySelector(".input-name");
+const todoInput = document.querySelector(".input-todo");
+const addBtn = document.querySelector(".btn-add");
+const allBtn = document.querySelector(".btn-all");
+const activeBtn = document.querySelector(".btn-active");
+const completedBtn = document.querySelector(".btn-completed");
+const clearCopletedBtn = document.querySelector(".btn-clear-completed");
+const todoList = document.querySelector(".todo-list");
+
+let todoListItems;
+
+getMockAPI();
+
+nameInput.value = localStorage.getItem("userName");
+nameInput.addEventListener("input", userName);
+
+function userName() {
+  localStorage.setItem("userName", nameInput.value);
+}
+
+function addDomTodo(content, id) {}
+
+async function getMockAPI() {
+  fetch("https://61c4e388f1af4a0017d9984f.mockapi.io/todos/")
+    .then((response) => response.json())
+    .then((data) => (todoListItems = data))
+    .then(() => todoListItems.map((data) => addDomTodo(data.content, data.id)))
+    .then(() => checkIsCompleted());
+}
+
+async function postMockAPI(content) {
+  fetch("https://61c4e388f1af4a0017d9984f.mockapi.io/todos/", {
+    method: "POST",
+    body: JSON.stringify({ content: content }),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  });
+}
+
+async function putMockAPI(id, isCompleted, content) {
+  fetch(`https://61c4e388f1af4a0017d9984f.mockapi.io/todos/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      content: content,
+      isCompleted: isCompleted,
+    }),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  });
+}
+
+async function deleteMockAPI(id) {
+  fetch(`https://61c4e388f1af4a0017d9984f.mockapi.io/todos/${id}`, {
+    method: "DELETE",
+  });
+}
